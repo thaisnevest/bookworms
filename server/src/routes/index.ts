5afed2e0dd212a48556f297fd761eb54a3fbd29a
Arthur <aarthuralvees@gmail.com>
2024-12-15 23:06:24 -0300

    arrumando o back

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,16 +1,9 @@
 import { Router } from 'express';
 
-import UserRouter from './UserRoutes';
-import AuthRouter from './AuthRoutes';
-import FileRouter from './FileRoutes';
-
 const router = Router();
 
-router.use('/user', UserRouter);
-router.use('/sessions', AuthRouter);
-router.use('/file', FileRouter);
 router.route('/').get((_, res) => {
-  res.status(200).send('Made with 💚 and &lt; &#x0002F; &gt; by CITi');
+  res.status(200).send('Made with 💚 and &lt; &#x0002F; &gt; by bookworms');
 });
 
 export default router;
