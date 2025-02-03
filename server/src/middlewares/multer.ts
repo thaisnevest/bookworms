adacaad9fb49a9b961bd9a38a23c7bd3aa0f3bf0
anaalbsantos <6030.anaalbuquerque@gmail.com>
2025-02-03 11:17:40 -0300

    main: fix eslint error

--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -1,11 +1,11 @@
-import multer from "multer";
+import multer from 'multer';
 
 const storage = multer.diskStorage({
-  filename: function(req, file, cb){
+  filename(req, file, cb) {
     cb(null, file.originalname);
-  }
+  },
 });
 
-const upload = multer({storage: storage});
+const upload = multer({ storage });
 
 export default upload;
