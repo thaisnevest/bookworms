7bc8350d09882fff10ae3c69f1d89961ba51bf87
thaisnevest <thaisneves@citi.org.br>
2025-02-16 00:36:42 -0300

    fix: fixed routes

--- server/src/routes/FeedRoutes.ts
+++ server/src/routes/FeedRoutes.ts
@@ -3,7 +3,11 @@ import FeedController from '../controllers/FeedController';
 
 const router = Router();
 
-router.get('/group/:groupId', FeedController.getFeed); // rota para pegar todos os posts do feed de um grupo
-router.get('/group/:groupId/user/:userId', FeedController.getUserFeed); // rota para pegar todos os posts de um usuário específico
+router.get('/', (_, res) => {
+  res.status(200).send('Feed API funcionando 🚀');
+});
+
+router.get('/groups/:groupId', FeedController.getFeed); // rota para pegar todos os posts do feed de um grupo
+router.get('/groups/:groupId/user/:userId', FeedController.getUserFeed); // rota para pegar todos os posts de um usuário específico
 
 export default router;
