9ae9c748ef1102202be5120c2510e3bf4d7027e7
thaisnevest <thaisneves@citi.org.br>
2025-03-08 21:14:21 -0300

    feat(front-end): added select-input component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col h-full justify-center items-center bg-black">
+    <div className="flex flex-1 flex-col h-full justify-center items-center bg-white">
       <h1 className="text-white text-4xl font-bold">bookworms.</h1>
+      {/* <SelectInput placeholder="Filtrar por usuário" options={["User1", "User2", "User3"]}/> */}
     </div>
   );
 }
--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -2,3 +2,4 @@ import ArrowInput from './arrow-input.svg';
 import GroupCoverImage from './group-cover.svg';
 
 export {ArrowInput, GroupCoverImage}; ;
+
