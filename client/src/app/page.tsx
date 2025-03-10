a2a1e6d0dda82fe552a3b4568a24f50ee441045b
thaisnevest <thaisneves@citi.org.br>
2025-03-10 10:54:07 -0300

    fix(components): removed components from page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className="flex flex-1 flex-col h-full justify-center items-center bg-white">
       <h1 className="text-white text-4xl font-bold">bookworms.</h1>
-      {/* <SelectInput placeholder="Filtrar por usuário" options={["User1", "User2", "User3"]}/> */}
     </div>
   );
 }
