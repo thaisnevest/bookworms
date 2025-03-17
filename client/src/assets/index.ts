25c9ff16ecc7ee5852be0bbffead4c06e41617bb
Giovanna <giovannacssv@hotmail.com>
2025-03-16 21:15:45 -0300

    feat(modal): implemented modal

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -2,6 +2,7 @@ import Worm from './worm.svg';
 import Woman2 from './woman2.svg';
 import Trash3 from './trash3.svg';
 import Edit3 from './edit3.svg';
+import Close from './close.svg'
 
 
-export { Worm , Woman2, Trash3, Edit3};
+export { Worm , Woman2, Trash3, Edit3, Close};
