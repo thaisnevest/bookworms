97499c38858d6927cb8a045107a81dc6619473b1
Giovanna <giovannacssv@hotmail.com>
2025-03-20 18:52:49 -0300

    added edit and delete comments

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -4,5 +4,6 @@ import Woman2 from './woman2.svg';
 import Trash3 from './trash3.svg';
 import Edit3 from './edit3.svg';
 import Close from './close.svg';
+import Check from './check.svg';
 
-export { Worm, WormBig, Woman2, Trash3, Edit3, Close };
+export { Worm, WormBig, Woman2, Trash3, Edit3, Close, Check };
