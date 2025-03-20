6671870e8396deb7391a1952af698f98ba95d909
Arthur <aarthuralvees@gmail.com>
2025-03-20 14:09:50 -0300

    feat(components): calendar and winners card components done

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -4,5 +4,6 @@ import { WinnerCard } from "./Winners-card";
 import { Layout } from './layout/page';
 import Modal from './modal';
 import { CommentInput } from './comments';
+import { Calendar } from './ui/calendar';
 
-export { TextInput, CustomButton, Layout, Modal, CommentInput, WinnerCard };
+export { TextInput, CustomButton, Layout, Modal, CommentInput, WinnerCard, Calendar };
