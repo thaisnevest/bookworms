721ff7989c1918ffc79d105ff3f3e7c29acfd172
Giovanna <giovannacssv@hotmail.com>
2025-03-21 22:46:02 -0300

    feat/page-title: added component title

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -5,5 +5,6 @@ import Trash3 from './trash3.svg';
 import Edit3 from './edit3.svg';
 import Close from './close.svg';
 import Check from './check.svg';
+import Arrow from './Arrow.svg'
 
-export { Worm, WormBig, Woman2, Trash3, Edit3, Close, Check };
+export { Worm, WormBig, Woman2, Trash3, Edit3, Close, Check, Arrow };
