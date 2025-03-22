1c303ccff248c970cb811805669d3521cfd1ba54
thaisnevest <thaisneves@citi.org.br>
2025-03-22 12:58:02 -0300

    fix: removed code test

--- client/src/app/Group/page.tsx
+++ client/src/app/Group/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 import { Layout } from 'components';
-import Ranking from 'components/ranking';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 export default function Profile() {
   const router = useRouter();
   const session = useSession({
-    // required: true,
+    required: true,
     onUnauthenticated() {
       router.replace('/Login');
     }
@@ -19,17 +18,6 @@ export default function Profile() {
     <Layout>
       <p className="text-borrowDark font-nunito">Group</p>
       <p className="text-borrowDark font-nunito">{user?.name}</p>
-      <Ranking users={[{
-        name: 'thais',
-        id: 'aa',
-        score: 0,
-        position: 0
-      }, {
-        name: 'victor',
-        id: '',
-        score: 0,
-        position: 0
-      }]} />
     </Layout>
   );
 }
