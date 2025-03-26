437ca93fbbc61745f82eafef43b5324bfff92c56
Giovanna <giovannacssv@hotmail.com>
2025-03-25 21:17:17 -0300

    feat/create-edit: integrate create/edit post screen

--- client/src/app/CreateEditPost/page.tsx
+++ client/src/app/CreateEditPost/page.tsx
@@ -1,169 +1,218 @@
 'use client';
 
 import * as React from 'react';
-import { Layout, TextInput } from "components";
-import { Button } from "components/ui/button";
-import PageTitle from "components/title";
-import { FileUpload } from "components";
-import api from "services/api";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { Layout, TextInput } from 'components';
+import { Button } from 'components/ui/button';
+import PageTitle from 'components/title';
+import { FileUpload } from 'components';
+import api from '../../services/api';
+import { useSession } from 'next-auth/react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useForm, SubmitHandler } from 'react-hook-form';
-
-
+import Ranking from 'components/ranking';
+import { useEffect } from 'react';
 
 type FormData = {
-    title: string;
-    body?: string; // Opcional (tem o ? no schema)
-    numPages: number;
-    authorId: string;
-    groupId: string; // Obrigatório (não tem ? no schema)
-    imageFile: File; // Para o upload (será convertido para string URL depois)
-  };
-  
+  title: string;
+  body?: string;
+  numPages: number;
+  groupId?: string;
+};
 
-export default function CreateEditPost() {
-    const router = useRouter();
-    const session = useSession({
-      required: true,
-      onUnauthenticated() {
-        router.replace('/Login');
-      }
-    });
-
-
-    const user = session.data?.user;
-    
-    //const { register, handleSubmit, setValue } = useForm<FormData>();
-    const [image, setImage] = React.useState<File | null>(null);
-
-    
-    // Adicione validações ao useForm
-const { register, handleSubmit, setValue, formState: { errors } } = useForm<FormData>({
-    defaultValues: {
-      numPages: 0
-    }
-  });
-  
-  // Modifique o handleSubmit para incluir melhor tratamento de erros
-  const handlePublish: SubmitHandler<FormData> = async (data) => {
-    try {
-      if (!user) throw new Error('Usuário não autenticado');
-  
-      const formData = new FormData();
-      
-      // Campos exatos como no schema.prisma
-      formData.append('title', data.title);
-      if (data.body) formData.append('body', data.body); // Opcional
-      formData.append('numPages', data.numPages.toString());
-      formData.append('authorId', user.id);
-      formData.append('groupId', data.groupId || user.groupId); // Obrigatório
-      
-      // Campo do arquivo (note que no schema é 'image' mas no FormData pode ser 'imageFile')
-      if (image) {
-        formData.append('imageFile', image); // O backend converterá para string URL
-      }
-  
-      // Debug: verifique todos os campos enviados
-      console.log('Dados sendo enviados:', {
-        title: data.title,
-        body: data.body,
-        numPages: data.numPages,
-        authorId: user.id,
-        groupId: data.groupId || user.groupId,
-        hasImage: !!image
-      });
-  
-      const response = await api.post('/posts', formData, {
-        headers: { 
-          'Content-Type': 'multipart/form-data',
-        }
-      });
-      
-      // Exemplo de utilização:
-      console.log('Post criado:', response.data);
-      router.push(`/post/${response.data.id}`); // Redireciona para o post criado
-  
-      
-    } catch (error) {
-        console.error('Erro ao criar o post', error);
-        // Adicione feedback visual para o usuário
-      }
-  };
-
-    const handleCancel = () => {
-        setValue('title', '');
-        setValue('numPages', 0);
-        setValue('body', '');
-        setImage(null);
-    };
-
-
-    return (
-        <Layout>
-            <div className="flex gap-20 justify-center">
-                <div className="flex flex-col items-center w-full max-w-[870px]">
-                    <div className="flex mt-9 justify-center w-full">
-                        <PageTitle title="Publicando no clube de livro" showBackButton={true} />
-                    </div>
-                    <form onSubmit={handleSubmit(handlePublish)} className="w-full">
-                        <div className="flex gap-3 mt-12 ml-[8px]">
-                        <TextInput 
-                        label="Título da Publicação" 
-                        type="text" 
-                        width="w-[502px]" 
-                        height="h-[50px]" 
-                        {...register('title', { required: 'Título é obrigatório' })}
-                        errorMessage={errors.title?.message}
-                        />
-                        <TextInput 
-                            label="N° de páginas lidas" 
-                            type="number" 
-                            width="w-[353px]" 
-                            height="h-[50px]" 
-                            {...register('numPages',  { required: 'Numero de Paginas é obrigatório' })}
-                            errorMessage={errors.numPages?.message}
-                        />
-                        </div>
-                        <div className="mt-5 ml-[8px] w-[870px]">
-                            <h2 className="text-borrow font-semibold font-nunito">Descrição</h2>
-                            <textarea
-                                {...register('body')}
-                                className="w-full h-[100px] p-2 border bg-white border-gray rounded-md focus-visible:ring-neutral-400 font-nunito text-borrowDark"
-                                rows={4}
-                            />
-                        </div>
-                        <div className="mt-8">
-                        <FileUpload
-  onFileSelect={(file) => {
-    // Verifique se o arquivo é válido antes de atualizar o estado
-    if (file) {
-      setImage(file);
-    }
-  }}
-  width={870}
-  height={246}
-/>
-                        </div>
-                        <div className="flex gap-3 p-8 w-full justify-end ml-[55px] mt-9">
-                            <Button 
-                                type="button"
-                                className="bg-gray w-[218px] h-[50px] font-nunito text-[20px] rounded-[10px]" 
-                                onClick={handleCancel}
-                            >
-                                Cancelar
-                            </Button>
-                            <Button 
-                                type="submit"
-                                className="bg-borrow text-white w-[218px] h-[50px] font-nunito text-[20px] rounded-[10px]" 
-                            >
-                               Publicar
-                            </Button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </Layout>
-    );
+interface RankingUser {
+  id: string;
+  name: string;
+  image?: string;
+  score: number;
+  position: number;
 }
 
+interface User {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  score: number;
+  bio: string;
+  image: string;
+  groupId: string;
+}
+
+interface ApiResponse {
+  data: User[];
+}
+
+export default function CreateEditPost() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const postId = searchParams.get('postId'); // Obtém o ID do post da URL, se existir
+  const session = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.replace('/Login');
+    }
+  });
+
+  const user = session.data?.user;
+  const [image, setImage] = React.useState<File | null>(null);
+  const [rankingData, setRanking] = React.useState<RankingUser[]>([]);
+  const { register, handleSubmit, setValue } = useForm<FormData>();
+
+  // Se for edição, buscar os dados do post
+  React.useEffect(() => {
+    if (postId) {
+      api
+        .get(`/posts/${postId}`)
+        .then((response) => {
+          const post = response.data;
+          setValue('title', post.title);
+          setValue('body', post.body || '');
+          setValue('numPages', post.numPages);
+        })
+        .catch((error) => console.error('Erro ao buscar post:', error));
+    }
+  }, [postId, setValue]);
+
+  const handlePublish: SubmitHandler<FormData> = async (data, event) => {
+    event?.preventDefault();
+    const formData = new FormData();
+
+    formData.append('title', data.title);
+    if (data.body) formData.append('body', data.body);
+    formData.append('numPages', data.numPages.toString());
+    if (user?.id) {
+      formData.append('authorId', user.id);
+    }
+    if (user?.groupId || data.groupId) {
+      formData.append('groupId', user?.groupId || data.groupId || '');
+    }
+    if (image) {
+      formData.append('image', image);
+    }
+
+    try {
+      if (postId) {
+        // Se estiver editando, fazer um PUT
+        await api.put(`/posts/${postId}`, formData, {
+          headers: { 'Content-Type': 'multipart/form-data' }
+        });
+      } else {
+        // Se estiver criando, fazer um POST
+        const response = await api.post('/posts/', formData, {
+          headers: { 'Content-Type': 'multipart/form-data' }
+        });
+        const newPostId = response.data.id;
+        router.push(`/PostDetails/${newPostId}`);
+      }
+    } catch (error) {
+      console.error('Erro ao acessar API:', error);
+    }
+  };
+
+  const handleCancel = () => {
+    setValue('title', '');
+    setValue('numPages', 0);
+    setValue('body', '');
+    setImage(null);
+  };
+
+  const handleFileSelect = (file: File) => {
+    setImage(file);
+  };
+
+  useEffect(() => {
+    const fetchRankingData = async () => {
+      try {
+        const res = await api.get<ApiResponse>(
+          `/score/ranking/${user?.groupId}`
+        );
+        const data = res.data.data;
+        const ranking = data.map((user, index) => ({
+          id: user.id,
+          name: user.name,
+          image: user.image,
+          score: user.score,
+          position: index + 1
+        }));
+        console.log('ranking', user?.groupId, data);
+        setRanking(ranking);
+      } catch (error) {
+        console.error('Erro ao buscar ranking:', error);
+      }
+    };
+
+    fetchRankingData();
+  }, [user, user?.groupId]);
+
+  return (
+    <Layout>
+      <div className="flex gap-20 justify-center pl-[150px]">
+        <div className="flex flex-col items-center w-full max-w-[870px]">
+          <div className="flex mt-9 justify-center w-full">
+            <PageTitle
+              title={
+                postId ? 'Editando publicação' : 'Publicando no clube de livro'
+              }
+              showBackButton={true}
+            />
+          </div>
+          <form onSubmit={handleSubmit(handlePublish)} className="w-full">
+            <div className="flex gap-3 mt-12 ml-[8px]">
+              <TextInput
+                label="Título da Publicação"
+                type="text"
+                width="w-[502px]"
+                height="h-[50px]"
+                {...register('title', { required: 'Título é obrigatório' })}
+              />
+              <TextInput
+                label="N° de páginas lidas"
+                type="number"
+                width="w-[353px]"
+                height="h-[50px]"
+                {...register('numPages', {
+                  required: 'Numero de Paginas é obrigatório'
+                })}
+              />
+            </div>
+            <div className="mt-5 ml-[8px] w-[870px]">
+              <h2 className="text-borrow font-semibold font-nunito">
+                Descrição
+              </h2>
+              <textarea
+                {...register('body')}
+                className="w-full h-[100px] p-2 border bg-white border-gray rounded-md focus-visible:ring-neutral-400 font-nunito text-borrowDark resize-none"
+                rows={4}
+              />
+            </div>
+            <div className="mt-8">
+              <FileUpload
+                onFileSelect={handleFileSelect}
+                width={870}
+                height={246}
+              />
+            </div>
+            <div className="flex gap-3 p-8 w-full justify-end ml-[55px] mt-9">
+              <Button
+                type="button"
+                className="bg-gray w-[218px] h-[50px] font-nunito text-[20px] rounded-[10px]"
+                onClick={handleCancel}
+              >
+                Cancelar
+              </Button>
+              <Button
+                type="submit"
+                className="bg-borrow text-white w-[218px] h-[50px] font-nunito text-[20px] rounded-[10px]"
+              >
+                {postId ? 'Salvar alterações' : 'Publicar'}
+              </Button>
+            </div>
+          </form>
+        </div>
+        <div className="mt-9 pl-[150px]">
+          <Ranking users={rankingData}></Ranking>
+        </div>
+      </div>
+    </Layout>
+  );
+}
--- server/src/routes/PostRoutes.ts
+++ server/src/routes/PostRoutes.ts
@@ -1,11 +1,12 @@
-import { Router } from "express";
-import { PostController } from "../controllers";
+import { Router } from 'express';
+import { PostController } from '../controllers';
+import upload from '../middlewares/multer';
 
 const router = Router();
 
-router.post("/", PostController.create);
-router.put("/:id", PostController.update);
-router.get("/:id", PostController.findById);
-router.delete("/:id", PostController.delete);
+router.post('/', upload.single('image'), PostController.create);
+router.put('/:id', upload.single('image'), PostController.update);
+router.get('/:id', PostController.findById);
+router.delete('/:id', PostController.delete);
 
 export default router;
